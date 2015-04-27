924c256510d0a26097aaa1d9c9de9cb689b911ee
Sarah Lee <sarahyoungahlee91@gmail.com>
2015-04-27 01:23:03 -0700

    Modified and styled the login page

--- index.php
+++ index.php
@@ -2,23 +2,31 @@
 <html>
 	<head>
 		<title>Login Page</title>
+		<link href="login.css" rel="stylesheet" type="type/css">
 	</head>
 	<body>
-		<header>
-		</header>
 		<main>
-			<form action="student.php" method="POST">STUDENT LOGIN:
-				<p> User: <input type="text" name="scwid"></p>
-				<input type="submit">
-			</form>
-			<form action="faculty.php" method="POST">Faculty LOGIN:
-				<p> User: <input type="text" name="fcwid"></p>
-				<input type="submit">
-			</form>
-			<form action="admin.php" method="POST">ADMIN LOGIN:
-				<p> User: <input type="text" name="acwid"></p>
-				<input type="submit">
-			</form>
+			<center>
+			<div class="login">
+				<p>Login</p>
+			</div>
+			<div class="form">
+				<form action="student.php" method="POST">Student:
+					<input type="text" name="scwid">
+					<input type="submit" id="submit">
+				</form>
+				<br>
+				<form action="faculty.php" method="POST">Faculty:
+					<input type="text" name="fcwid">
+					<input type="submit" id="submit">
+				</form>
+				<br>
+				<form action="admin.php" method="POST">Admin:
+					<input type="text" name="acwid">
+					<input type="submit" id="submit">
+				</form>
+			</div>
+			</center>
 		</main>
 	</body>
 </html>
