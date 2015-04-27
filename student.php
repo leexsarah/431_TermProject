8c7a0120d44068678100aa462327f30c44c0a3fe
Christopher Danan <chrisddan@gmail.com>
2015-04-27 14:37:31 -0700

    In the process of having 'view grade' button show up.

--- student.php
+++ student.php
@@ -41,11 +41,7 @@
 		mysqli_close($link);
 	}
 
-	echo "IT WORKS";
-
 	$_SESSION["scwid"] = $scwid;
-
-	echo $fname . " " . $lname . " " . $scwid;
 ?>
 	<!doctype html>
 	<html>
@@ -54,5 +50,48 @@
 			<link href="style.css" rel="stylesheet" type="type/css">
 		</head>
 		<body>
+			<h1>Welcome <?php echo $fname ?></h1>
+			<?php
+				echo "<table valign='top' cellpadding='5'>";
+				echo "<caption>Your Profile:</caption>";
+				echo "<tr>";
+			    echo "<td>Name:</td><td>".$fname." ".$lname."</td>";
+				echo "</tr>";
+				echo "<tr>";
+			    echo "<td>CWID:</td><td>".$scwid."</td>";
+				echo "</tr>";
+			    echo "</table>";
+			 ?>
+
+			 <table valign='top' cellpadding='5'>
+			 	<caption>Your Class List</caption>
+				<?php
+					$link = mysqli_connect('localhost', 'root', 'root', 'school') or 
+							mysqli_connect('localhost', 'root', '', 'school');
+
+				 	$query = "select fk_course_id, fk_section_number from student_section where fk_scwid = " . $scwid . ";";
+
+					$result = $link->query($query) or die("ERROR:" . mysqli_error($link));
+
+					while($row = mysqli_fetch_array($result)){
+
+						$fk_course_id = $row["fk_course_id"];
+						$fk_section_number = $row["fk_section_number"];
+
+						echo "<tr>";
+						echo "<td>" . $fk_course_id . "-" . $fk_section_number . "</td>";
+						echo "<td>";
+						echo "<form action='view_grades.php' method='POST'>";
+						echo "<input type='hidden' name='fk_course_id' value='".$fk_course_id."' />";
+						echo "<input type='submit' id='submit>";
+						echo "</form>";
+						echo "</td>";
+						echo "</tr>";
+					}
+
+					$result->free();
+					mysqli_close($link);
+				?>
+			</table>
 		</body>
 </html>
