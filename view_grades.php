1666a2260bd241763ce25a34cbdc901d3a807918
Christopher Danan <chrisddan@gmail.com>
2015-05-08 00:22:38 -0700

    Updated grades page to work with new login system.

--- view_grades.php
+++ view_grades.php
@@ -6,47 +6,48 @@
 	</head>
 	<body>
 	<center>
-<?php
-	session_start();
-	$course = $_POST["fk_course_id"];
-	$student = $_SESSION["scwid"];
+	<?php
+		session_start();
 
-	$link = mysqli_connect('localhost', 'root', '', 'school');
-				
-	$select = "SELECT * ";
-	$from = "FROM student ";
-	$join = "INNER JOIN course_grades ";
-	$where = "WHERE student.scwid = ".$student." AND student.scwid = course_grades.fk_scwid AND course_grades.fk_course_id = '".$course."';";
-	$query = $select.$from.$join.$where;
-	$result = $link->query($query);
+		$course = $_POST["fk_course_id"];
+		$student = $_SESSION["cwid"];
 
-	$row = mysqli_fetch_array($result);
+		include "create_database_link.php";
+					
+		$select = "SELECT * ";
+		$from = "FROM student ";
+		$join = "INNER JOIN course_grades ";
+		$where = "WHERE student.scwid = ".$student." AND student.scwid = course_grades.fk_scwid AND course_grades.fk_course_id = '".$course."';";
+		$query = $select.$from.$join.$where;
+		$result = $link->query($query);
 
-	$fk_course_id = $row['fk_course_id'];
-    $fk_section_number = $row['fk_section_number'];
-    $midterm_score = $row['midterm_score'];
-    $final_score = $row['final_score'];
-    $term_project_score = $row['term_project_score'];
+		$row = mysqli_fetch_array($result);
 
-	echo "<table valign='top' cellpadding='5'>";
-	echo "<caption>Grades:</caption>";
-	echo "<tr>";
-    echo "<td>Course:</td><td>".$fk_course_id."-".$fk_section_number."</td>";
-	echo "</tr>";
-	echo "<tr>";
-    echo "<td>Midterm Exam Score:</td><td>".$midterm_score."%</td>";
-	echo "</tr>";
-	echo "<tr>";
-    echo "<td>Final Exam Score:</td><td>".$final_score."%</td>";
-	echo "</tr>";
-	echo "<tr>";
-    echo "<td>Term Project Score:</td><td>".$term_project_score."%</td>";
-	echo "</tr>";
-	echo "</tr>";
-    echo "</table>";
+		$fk_course_id = $row['fk_course_id'];
+	    $fk_section_number = $row['fk_section_number'];
+	    $midterm_score = $row['midterm_score'];
+	    $final_score = $row['final_score'];
+	    $term_project_score = $row['term_project_score'];
 
-    mysqli_close($link);
-?>
+		echo "<table valign='top' cellpadding='5'>";
+		echo "<caption>Grades:</caption>";
+		echo "<tr>";
+	    echo "<td>Course:</td><td>".$fk_course_id."-".$fk_section_number."</td>";
+		echo "</tr>";
+		echo "<tr>";
+	    echo "<td>Midterm Exam Score:</td><td>".$midterm_score."%</td>";
+		echo "</tr>";
+		echo "<tr>";
+	    echo "<td>Final Exam Score:</td><td>".$final_score."%</td>";
+		echo "</tr>";
+		echo "<tr>";
+	    echo "<td>Term Project Score:</td><td>".$term_project_score."%</td>";
+		echo "</tr>";
+		echo "</tr>";
+	    echo "</table>";
+
+	    mysqli_close($link);
+	?>
 	</center>
 	</body>
 </html>
