6a41a88c2f70a3f9a8bfa8131c477d12b15bb173
Christopher Danan <chrisddan@gmail.com>
2015-05-07 22:55:03 -0700

    Changed $password variable name to $pword.

--- create_database_link.php
+++ create_database_link.php
@@ -1,13 +1,13 @@
 <?php
 
 	//Reference for single connection file: http://php.about.com/od/phpwithmysql/qt/mysql_connect.htm
-	
+
 	$host = "localhost";
 	$user = "root";
-	$password = "root";
+	$pword = "root";
 	$db = "school";
 
-	$link = mysqli_connect($host, $user, $password, $db);
+	$link = mysqli_connect($host, $user, $pword, $db);
 
 	if(mysqli_connect_errno()){
 		echo "Connection failed: " . mysqli_connect_error();
