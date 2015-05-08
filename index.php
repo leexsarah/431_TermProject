b418ec8c63e15eb689b81b8f94693242b3e9513d
Sarah Lee <sarahyoungahlee91@gmail.com>
2015-05-08 11:26:01 -0700

    modified the css

--- index.php
+++ index.php
@@ -10,44 +10,16 @@
 	</head>
 	<body>
 		<main>
-			<center>
 			<div class="login">
 				<p>Login</p>
 
 				<div class="login-form">
 					<form action="login.php" method="POST">
-						<input type="text" id="username-text" name="username" />
-						<input type="text" id="password-text" name="password" />
+						<p>Username: <input type="text" id="username-text" name="username" /></p>
+						<p>Password: <input type="text" id="password-text" name="password" /></p>
 						<input type="submit" id="submitButton" />
 					</form>
 				</div>
-			</div>
-
-			<div class="account-list">
-				<p>This is a list of all the accounts on the site.
-					This is listed here for convenience purposes.</p>
-				<table>
-					<tr>
-						<th>USERNAME</th>
-						<th>PASSWORD</th>
-						<th>CWID</th>
-					</tr>
-					<?php
-						include "create_database_link.php";
-
-						$query = "SELECT * FROM account;";
-						$result = $link->query($query) or die("ERROR: Query did not complete");
-
-						while($row = mysqli_fetch_array($result)){
-							echo "<tr>";
-							echo "<td>" . $row["username"] . "</td><td> " . $row["password"] . "</td><td> " . $row["fk_cwid"] . "</td>";
-							echo "</tr>";
-						}
-
-						$result->free();
-						mysqli_close($link);
-					?>
-				</ul>
 			</div>
 			<?php
 				//Output login error.
@@ -58,7 +30,6 @@
 				unset($_SESSION["loginError"]);
 			?>
 			</div>
-			</center>
 		</main>
 	</body>
 </html>
