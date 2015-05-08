43abe51ecea1d4b9a2c7f9b299c82f9e26014f8e
Christopher Danan <chrisddan@gmail.com>
2015-05-08 11:44:07 -0700

    Changed hyperlink for course_list anchor.

--- Student_files/header.php
+++ Student_files/header.php
@@ -2,6 +2,6 @@
 	echo "<ul>";
 	echo "<li><a href='profile.php'>Profile</a></li>";
 	echo "<li><a href='student_view_class_list.php'>View Your Classes</a></li>";
-	echo "<li><a href='student_view_courses.php'>Enroll</a></li>";
+	echo "<li><a href='student_course_list.php'>Enroll</a></li>";
 	echo "</ul>";
 ?>
