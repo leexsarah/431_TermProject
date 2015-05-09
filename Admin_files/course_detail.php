3a785ce04719c392bb7697e5bd934ccfd0acff2e
Christopher Danan <chrisddan@gmail.com>
2015-05-08 20:30:38 -0700

    Add code that allows admin to view course information (e.g. section numbers, instructors, etc.).

--- Admin_files/course_detail.php
+++ Admin_files/course_detail.php
@@ -14,7 +14,45 @@
 <body>
 	<h1>Details for <?php echo $courseID . "-" . $courseName; ?></h1>
 
+	<?php
+		include "../create_database_link.php";
 
+		$query = "SELECT section_number, days, start_time, end_time, room, seats_available, total_seats, fname, lname FROM section, csuf_member WHERE fk_course_id='$courseID' AND cwid = instructor;";
+		$result = $link->query($query) or die("ERROR: " . mysqli_error($link));
+
+		echo "<table>";
+		echo "<tr>";
+		echo "<th>Section #</th>";
+		echo "<th>Instructor</th>";
+		echo "<th>Days</th>";
+		echo "<th>Start Time</th>";
+		echo "<th>End Time</th>";
+		echo "<th>Room Number</th>";
+		echo "<th>Available Seats</th>";
+		echo "<th>Total Seats</th>";
+		echo "</tr>";
+
+		while($row = mysqli_fetch_array($result)){
+			$sectionNumber = $row["section_number"];
+			$instructor = $row["fname"] . " " . $row["lname"];
+			$days = $row["days"];
+			$start = $row["start_time"];
+			$end = $row["end_time"];
+			$room = $row["room"];
+			$availableSeats = $row["seats_available"];
+			$totalSeats = $row["total_seats"];
+
+			echo "<tr>";
+			echo "<td>$sectionNumber</td>";
+			echo "<td>$instructor</td>";
+			echo "<td>$days</td>";
+			echo "<td>$start</td>";
+			echo "<td>$end</td>";
+			echo "<td>$room</td>";
+			echo "<td>$availableSeats</td>";
+			echo "<td>$totalSeats</td>";
+		}
+	?>
 </body>
 </html>
 
